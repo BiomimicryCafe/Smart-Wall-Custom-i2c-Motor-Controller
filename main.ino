dcf082f72b60b9655c99bdb098a2af6058ff82af
BiomimicryCafe <110191082+BiomimicryCafe@users.noreply.github.com>
2022-11-10 14:43:26 -0600

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,8 +1,20 @@
 #include <Arduino.h>
 #include "Wire.h"
+#define solenoidPin 10;
+#define pumpPin 9;
+Wire comm;
+
+void handleInput() {
+ if (wire.available()) {
+   
+ }
+}
 
 void setup() {
-
+  comm.begin(0x0f);
+  pinMode(solenoidPin, OUTPUT);
+  pinMode(pumpPin, OUTPUT);
+  comm.onRecieve(handleInput);
 }
 
 void loop() {
